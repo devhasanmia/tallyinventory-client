fd70b40ef65fc58c8a9db301b755bee500713eba
devhasanmia <dev.hasanmia@gmail.com>
2025-04-06 10:35:26 +0600

    added menu item

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,48 +1,64 @@
-import React from 'react';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
-
+import React from "react";
+import { Layout, Menu } from "antd";
+const { Header, Content, Sider } = Layout;
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { IoMdSettings } from "react-icons/io";
+import { IoPeople } from "react-icons/io5";
+import { BsPersonLinesFill, BsPersonFillCheck } from "react-icons/bs";
 const items = [
-    {
-        key: "s",
-        label: `Dashboard`,
-
-    }
-]
+  {
+    key: "dashboard",
+    label: `Dashboard`,
+    icon: <TbLayoutDashboardFilled/>
+  },
+  {
+    key: "People",
+    label: `People`,
+    icon: <IoPeople/>,
+    children: [
+      {
+        key: "Customer",
+        label: "Customer",
+        icon: <BsPersonFillCheck/>
+      },
+      {
+        key: "dealer",
+        label: "Dealer",
+        icon: <BsPersonLinesFill/>
+      },
+    ],
+  },
+  {
+    key: "settings",
+    label: `Settings`,
+    icon:  <IoMdSettings/>
+    
+  },
+];
 
 const AdminLayout: React.FC = () => {
   return (
-    <Layout  style={{height:"100vh"}}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-      >
-        <div style={{
-          height: "32px",
-          margin: "16px",
-          textAlign: "center",
-          color: "#fff",
-          fontSize: "20px",
-          fontWeight: "bold",
-        }}>Tally Inv.</div>
+    <Layout style={{ height: "100vh" }}>
+      <Sider breakpoint="lg" collapsedWidth="0">
+        <div
+          style={{
+            height: "32px",
+            margin: "16px",
+            textAlign: "center",
+            color: "#fff",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          Tally Inv.
+        </div>
         <Menu theme="dark" mode="inline" items={items} />
       </Sider>
       <Layout>
-        <Header />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-            }}
-          >
-            content
-          </div>
+        <Header></Header>
+        <Content className="p-5">
+          <div>content</div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Tally Inventory ©{new Date().getFullYear()} Created by MD. HASAN MIA
-        </Footer>
       </Layout>
     </Layout>
   );
