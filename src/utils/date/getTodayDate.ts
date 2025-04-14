cc8cddfce77bb2bec7b1b18ce309128184c870e2
devhasanmia <dev.hasanmia@gmail.com>
2025-04-14 15:28:20 +0600

    refactor sidebar

---
+++ src/utils/date/getTodayDate.ts
@@ -0,0 +1,14 @@
+export const getTodayDate = (
+    locale: string = 'en-GB',
+    options: Intl.DateTimeFormatOptions = {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }
+  ): string => {
+    const today: Date = new Date();
+    return today.toLocaleDateString(locale, options);
+  };
+
+
+
